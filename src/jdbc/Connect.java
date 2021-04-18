e5b7f6bf719b84c86a73ddc9507bfe68863d052f
Rajesh <rajeshgillerla@gmai.com>
2021-04-18 19:27:27 +0530

    first

---
+++ src/jdbc/Connect.java
@@ -0,0 +1,42 @@
+package jdbc;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/Connect")
+public class Connect extends HttpServlet {
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException
+	{
+		res.setContentType("text/html");
+	PrintWriter out=res.getWriter();
+	String name=req.getParameter("name");
+	int id=Integer.parseInt(req.getParameter("id"));
+	String address=req.getParameter("addres");
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","rajesh");
+		PreparedStatement pes=con.prepareStatement("insert into person values(?,?,?)");
+		pes.setString(1,name);
+		pes.setInt(2,id);
+		pes.setString(3,address);
+		int i=pes.executeUpdate();
+		if(i>0)
+		out.println("inserted database");
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	out.close();
+	}
+	}
+
